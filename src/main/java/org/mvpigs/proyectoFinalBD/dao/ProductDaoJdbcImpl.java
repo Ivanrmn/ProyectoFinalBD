56640cae3c2eca406c75723a456dd200ce7d67ea
Unknown <ivanrmnh@gmail.com>
2018-05-25 18:06:05 +0200

    Cambios en consultas sql

--- src/main/java/org/mvpigs/proyectoFinalBD/dao/ProductDaoJdbcImpl.java
+++ src/main/java/org/mvpigs/proyectoFinalBD/dao/ProductDaoJdbcImpl.java
@@ -21,7 +21,7 @@ import java.util.List;
         @Override
         @RequestMapping("/insert")
         public void insert(ProductDto productDto) {
-            String sql = "INSERT INTO FRUITS(ID_FRUIT, NAME, DESCRIPTION, PRICE_PER_KG, DATE_CREATED values(?,?,?,?,?)";
+            String sql = "INSERT INTO PRODUCTS(ID_PRODUCT, NAME, DESCRIPTION, PRICE, values(?,?,?,?)";
             jdbcTemplate.update(sql, productDto.getId(), productDto.getName(), productDto.getDescription(), productDto.getPrice());
         }
 
@@ -29,39 +29,37 @@ import java.util.List;
         @RequestMapping("/update")
         public void update(ProductDto productDto) {
             jdbcTemplate.update(
-                    "UPDATE FRUITS SET ID_FRUIT=?, NAME=?, DESCRIPTION=?, PRICE_PER_KG=?, DATE_CREATED=? ");
+                    "UPDATE PRODUCTS SET ID_PRODUCT=?, NAME=?, DESCRIPTION=?, PRICE=?");
         }
 
         @Override
         @RequestMapping("/delete")
         public void delete(ProductDto productDto) {
-            jdbcTemplate.update("DELETE FROM FRUITS WHERE ID_FRUIT = ?", new Object[]{productDto.getId()});
+            jdbcTemplate.update("DELETE FROM PRODUCTS WHERE ID_PRODUCT = ?", new Object[]{productDto.getId()});
         }
 
         @Override
         public String getProduct(Long id) {
-            String sql = "SELECT NAME FROM FRUITS WHERE ID_FRUIT = ?";
+            String sql = "SELECT NAME FROM PRODUCTS WHERE ID_PRODUCTS = ?";
             return jdbcTemplate.queryForObject(sql, new Object[]{id}, String.class);
         }
 
         @Override
         public List<ProductDto> list() {
-            String sql = "SELECT * FROM FRUITS ORDER BY NAME";
-            List<ProductDto> fruitList = jdbcTemplate.query(sql, (resultSet, i) -> {
-                // crear y asignar un nuevo FruitDto
+            String sql = "SELECT * FROM PRODUCTS ORDER BY NAME";
+            List<ProductDto> productList = jdbcTemplate.query(sql, (resultSet, i) -> {
                 try {
                     ProductDto emp = new ProductDto();
-                    emp.setId(resultSet.getLong("ID_FRUIT"));
+                    emp.setId(resultSet.getLong("ID_PRODUCT"));
                     emp.setName(resultSet.getString("NAME"));
                     emp.setDescription(resultSet.getString("DESCRIPTION"));
-                    emp.setPrice(resultSet.getDouble("PRICE_PER_KG"));
+                    emp.setPrice(resultSet.getDouble("PRICE"));
 
                     return emp;
                 } catch (SQLException e) {
                     throw new SQLException("Error interno: " + e.getMessage(), e.getCause());
                 }
             });
-            return fruitList;
+            return productList;
         }
     }
-}
