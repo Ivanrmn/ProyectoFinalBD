8b6e485db3a588e1487d81210b527aba03fd0db7
Unknown <ivanrmnh@gmail.com>
2018-05-25 18:12:18 +0200

    Interfaz servicio

--- src/main/java/org/mvpigs/proyectoFinalBD/service/ProductService.java
+++ src/main/java/org/mvpigs/proyectoFinalBD/service/ProductService.java
@@ -1,8 +1,6 @@
 package org.mvpigs.proyectoFinalBD.service;
 
 public interface ProductService {
-    String getTransformedModelo(String firstWord, String secondWord) throws RookieProjectException;
-    Modelo createModel(String firstWord, String secondWord);
-    String getTransformed(Modelo modelo) throws RookieProjectException;
+
 }
 
