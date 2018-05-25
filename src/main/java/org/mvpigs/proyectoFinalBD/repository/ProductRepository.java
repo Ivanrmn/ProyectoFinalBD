894cca32e09e5ccfd4b560ed60f14b44c3577176
Unknown <ivanrmnh@gmail.com>
2018-05-25 18:19:06 +0200

    Repositorio creado

---
+++ src/main/java/org/mvpigs/proyectoFinalBD/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package org.mvpigs.proyectoFinalBD.repository;
+
+import org.mvpigs.proyectoFinalBD.model.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findByDescription(String description);
+    List<Product> findByName(String name);
+    List<Product> findByPrice(Double price);
+}
